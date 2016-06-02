89c42e59af5db80b4ab7a6b1a980d6a037d2637d
Sergio Avalos <sergio@spotify.com>
2016-06-02 19:10:21 +0200

    Replaces tabs for spaces

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -4,140 +4,140 @@ var PubSub = require('../src/js/PubSub.js');
 var fs = require('fs');
 
 describe('Board', function() {
-	var board, fullBoard, containerElem;
+  var board, fullBoard, containerElem;
 
-	beforeEach(function() {
-		document.write('<html><body><div id="board-container" class="board-container"></div></body></html>');
+  beforeEach(function() {
+    document.write('<html><body><div id="board-container" class="board-container"></div></body></html>');
 
-		board = new Board(
-			'#board-container',
-			[
-			       /*  0  , 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  */
-			/* 0 */ null,null,null,null,null,null,null,null,null,
-			/* 1 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
-			/* 2 */ null,null,null,null,null,null,null,null,null,
-			/* 3 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
-			/* 4 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
-			/* 5 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
-			/* 6 */ null,null,null,null,null,null,null,null,null,
-			/* 7 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
-			/* 8 */ null,null,null,null,null,null,null,null,null,
-			]
-		);
-	});
+    board = new Board(
+      '#board-container',
+      [
+             /*  0  , 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  */
+      /* 0 */ null,null,null,null,null,null,null,null,null,
+      /* 1 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
+      /* 2 */ null,null,null,null,null,null,null,null,null,
+      /* 3 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
+      /* 4 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
+      /* 5 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
+      /* 6 */ null,null,null,null,null,null,null,null,null,
+      /* 7 */  1  , 2  , 3  , 4  , 5  , 6  , 7  , 8  , 9  ,
+      /* 8 */ null,null,null,null,null,null,null,null,null,
+      ]
+    );
+  });
 
-	describe('#init', function() {
-		it('should return a view for board',  function() {
-			// REVIEW: There should be a better way to test a view
-			board.init(PubSub);
-			containerElem = document.getElementById('board-container');
+  describe('#init', function() {
+    it('should return a view for board',  function() {
+      // REVIEW: There should be a better way to test a view
+      board.init(PubSub);
+      containerElem = document.getElementById('board-container');
 
-			expect( containerElem.querySelectorAll('div.sudoku-cell').length ).toBe(81);
-		});
-	});
+      expect( containerElem.querySelectorAll('div.sudoku-cell').length ).toBe(81);
+    });
+  });
 
-	describe('#selectCell', function() {
-		it('should select only one cell', function() {
-			board.selectCell(0);
-			expect( board.selectedIndex ).toBe(0);
+  describe('#selectCell', function() {
+    it('should select only one cell', function() {
+      board.selectCell(0);
+      expect( board.selectedIndex ).toBe(0);
 
-			board.selectCell(100);
-			expect( board.selectedIndex ).toBe(0);
-		});
-	});
+      board.selectCell(100);
+      expect( board.selectedIndex ).toBe(0);
+    });
+  });
 
-	describe('#clearSelectedCell', function() {
-		it('should clear all the values in the cells', function() {
-			board.selectCell(0);
-			board.clearSelectedCell();
-			// REVIEW: We need a method to get cells from Board (probably get selected cell???)
-			// REVIEW: We need a method to get selectedIndex
-			expect( board.cells[board.selectedIndex].getValue() ).toBe(null);
-		});
-	});
+  describe('#clearSelectedCell', function() {
+    it('should clear all the values in the cells', function() {
+      board.selectCell(0);
+      board.clearSelectedCell();
+      // REVIEW: We need a method to get cells from Board (probably get selected cell???)
+      // REVIEW: We need a method to get selectedIndex
+      expect( board.cells[board.selectedIndex].getValue() ).toBe(null);
+    });
+  });
 
-	describe('#getSelectedCell', function() {
-		it('should return the selected cell', function() {
-			board.selectCell(9);
-			var selectedCell = board.getSelectedCell();
+  describe('#getSelectedCell', function() {
+    it('should return the selected cell', function() {
+      board.selectCell(9);
+      var selectedCell = board.getSelectedCell();
 
-			expect( selectedCell.getValue() ).toBe(1);
-			expect( selectedCell.editable ).toBe(false);
-		});
+      expect( selectedCell.getValue() ).toBe(1);
+      expect( selectedCell.editable ).toBe(false);
+    });
 
-		it('should return null when nothing is selected', function() {
-			expect( board.getSelectedCell() ).toBe(null);
-		});
-	});
+    it('should return null when nothing is selected', function() {
+      expect( board.getSelectedCell() ).toBe(null);
+    });
+  });
 
-	describe('#setValueOnSelectedCell', function() {
-		it('should return set the value on the selected cell', function() {
-			board.selectCell(0);
-			board.setValueOnSelectedCell(4);
-			var cell = board.getSelectedCell();
+  describe('#setValueOnSelectedCell', function() {
+    it('should return set the value on the selected cell', function() {
+      board.selectCell(0);
+      board.setValueOnSelectedCell(4);
+      var cell = board.getSelectedCell();
 
-			expect( cell.getValue() ).toEqual(4);
-		});
-	});
+      expect( cell.getValue() ).toEqual(4);
+    });
+  });
 
-	describe('#isComplete', function() {
-		it('should return true for a full board', function() {
-			for (var i = 0, cells = []; i < 82; i++) {
-				cells.push(9); // Whatever value....
-			}
-			fullBoard = new Board('#board-container', cells);
+  describe('#isComplete', function() {
+    it('should return true for a full board', function() {
+      for (var i = 0, cells = []; i < 82; i++) {
+        cells.push(9); // Whatever value....
+      }
+      fullBoard = new Board('#board-container', cells);
 
-			expect( fullBoard.isComplete() ).toBe(true);
-		});
+      expect( fullBoard.isComplete() ).toBe(true);
+    });
 
-		it('should return false for a uncomplete board', function() {
-			expect( board.isComplete() ).toBe(false);
-		});
-	});
+    it('should return false for a uncomplete board', function() {
+      expect( board.isComplete() ).toBe(false);
+    });
+  });
 
-	describe('#getCurrentColumnValues', function() {
-		it('should return the current values of the selected column', function() {
-			board.selectCell(0);
-			expect( board.getCurrentRowValues() ).toEqual([null,null,null,null,null,null,null,null,null]);
+  describe('#getCurrentColumnValues', function() {
+    it('should return the current values of the selected column', function() {
+      board.selectCell(0);
+      expect( board.getCurrentRowValues() ).toEqual([null,null,null,null,null,null,null,null,null]);
 
-			board.selectCell(40);
-			expect( board.getCurrentRowValues() ).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-		});
+      board.selectCell(40);
+      expect( board.getCurrentRowValues() ).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
 
-	});
+  });
 
-	describe('#getCurrentRowValues', function() {
-		it('should return the current values of the selected row', function() {
-			board.selectCell(0);
-			expect( board.getCurrentColumnValues() ).toEqual([null, 1, null, 1, 1, 1, null, 1,null]);
+  describe('#getCurrentRowValues', function() {
+    it('should return the current values of the selected row', function() {
+      board.selectCell(0);
+      expect( board.getCurrentColumnValues() ).toEqual([null, 1, null, 1, 1, 1, null, 1,null]);
 
-			board.selectCell(4);
-			expect( board.getCurrentColumnValues() ).toEqual([null, 5, null, 5, 5, 5, null, 5, null]);
+      board.selectCell(4);
+      expect( board.getCurrentColumnValues() ).toEqual([null, 5, null, 5, 5, 5, null, 5, null]);
 
-		});
+    });
 
-	});
+  });
 
-	describe('#getCurrentMatrixValues', function() {
-		it('should return the current values of the selected matrix', function() {
-			board.selectCell(10);
-			expect( board.getCurrentMatrixValues() ).toEqual([null, null, null, 1, 2, 3, null, null, null]);
+  describe('#getCurrentMatrixValues', function() {
+    it('should return the current values of the selected matrix', function() {
+      board.selectCell(10);
+      expect( board.getCurrentMatrixValues() ).toEqual([null, null, null, 1, 2, 3, null, null, null]);
 
-			board.selectCell(0);
-			expect( board.getCurrentMatrixValues() ).toEqual([null, null, null, 1, 2, 3, null, null, null]);
+      board.selectCell(0);
+      expect( board.getCurrentMatrixValues() ).toEqual([null, null, null, 1, 2, 3, null, null, null]);
 
-			board.selectCell(19);
-			expect( board.getCurrentMatrixValues() ).toEqual([null, null, null, 1, 2, 3, null, null, null]);
+      board.selectCell(19);
+      expect( board.getCurrentMatrixValues() ).toEqual([null, null, null, 1, 2, 3, null, null, null]);
 
-			board.selectCell(30);
-			expect( board.getCurrentMatrixValues() ).toEqual([4, 5, 6, 4, 5, 6, 4, 5, 6]);
+      board.selectCell(30);
+      expect( board.getCurrentMatrixValues() ).toEqual([4, 5, 6, 4, 5, 6, 4, 5, 6]);
 
-			board.selectCell(40);
-			expect( board.getCurrentMatrixValues() ).toEqual([4, 5, 6, 4, 5, 6, 4, 5, 6]);
+      board.selectCell(40);
+      expect( board.getCurrentMatrixValues() ).toEqual([4, 5, 6, 4, 5, 6, 4, 5, 6]);
 
-			board.selectCell(50);
-			expect( board.getCurrentMatrixValues() ).toEqual([4, 5, 6, 4, 5, 6, 4, 5, 6]);
-		});
+      board.selectCell(50);
+      expect( board.getCurrentMatrixValues() ).toEqual([4, 5, 6, 4, 5, 6, 4, 5, 6]);
+    });
 
-	});
+  });
 });
--- src/js/Board.js
+++ src/js/Board.js
@@ -1,179 +1,179 @@
 var Cell = require('./Cell');
 
 var Board = function Board(idContainer, cellsValues, duplicationDetector){
-	this.boardElem = null;
-	this.containerElem = document.querySelector(idContainer);
-	this.cells = [];
-	this.resolved = false;
-	this.selectedIndex = null;
-	this.detector = duplicationDetector;
+  this.boardElem = null;
+  this.containerElem = document.querySelector(idContainer);
+  this.cells = [];
+  this.resolved = false;
+  this.selectedIndex = null;
+  this.detector = duplicationDetector;
 
-	for (var index = 0; index < 81; index++) {
-		if (typeof cellsValues != 'undefined' && cellsValues[index] !== null) {
-			this.cells.push(new Cell(index, cellsValues[index]));
-		} else {
-			this.cells.push(new Cell(index));
-		}
-	}
+  for (var index = 0; index < 81; index++) {
+    if (typeof cellsValues != 'undefined' && cellsValues[index] !== null) {
+      this.cells.push(new Cell(index, cellsValues[index]));
+    } else {
+      this.cells.push(new Cell(index));
+    }
+  }
 
 };
 
 Board.prototype.init = function init(PubSub) {
-	this.boardElem = document.createElement('div');
-	this.boardElem.classList.add('sudoku-board');
-	this.containerElem.appendChild(this.boardElem);
+  this.boardElem = document.createElement('div');
+  this.boardElem.classList.add('sudoku-board');
+  this.containerElem.appendChild(this.boardElem);
 
-	var cellListFragment = document.createDocumentFragment();
-	this.cells.forEach(function(cell) {
-		cellListFragment.appendChild(cell.getElement());
-	});
-	this.boardElem.appendChild(cellListFragment);
+  var cellListFragment = document.createDocumentFragment();
+  this.cells.forEach(function(cell) {
+    cellListFragment.appendChild(cell.getElement());
+  });
+  this.boardElem.appendChild(cellListFragment);
 
-	// Setup events
-	this.containerElem.addEventListener('click', this.onBoardClicked.bind(this));
-	PubSub.subscribe('on-clear-key-pressed', this.onClearKeyPressed.bind(this));
-	PubSub.subscribe('on-number-key-pressed', this.onNumberKeyPressed.bind(this));
+  // Setup events
+  this.containerElem.addEventListener('click', this.onBoardClicked.bind(this));
+  PubSub.subscribe('on-clear-key-pressed', this.onClearKeyPressed.bind(this));
+  PubSub.subscribe('on-number-key-pressed', this.onNumberKeyPressed.bind(this));
 };
 
 
 Board.prototype.getCurrentColumnValues = function getCurrentColumnValues() {
-	var allValues = [];
-	var columnIndex = this.selectedIndex % 9;
-	while(columnIndex < 81) {
-		allValues.push(this.cells[columnIndex].getValue());
-		columnIndex += 9;
-	}
+  var allValues = [];
+  var columnIndex = this.selectedIndex % 9;
+  while(columnIndex < 81) {
+    allValues.push(this.cells[columnIndex].getValue());
+    columnIndex += 9;
+  }
 
-	return allValues;
+  return allValues;
 };
 
 Board.prototype.getCurrentRowValues = function getCurrentRowValues() {
-	var allValues = [];
-	var row = Math.floor(this.selectedIndex / 9);
-	var rowStart = row * 9;
-	var rowEnd = rowStart + 9;
+  var allValues = [];
+  var row = Math.floor(this.selectedIndex / 9);
+  var rowStart = row * 9;
+  var rowEnd = rowStart + 9;
 
-	for (var i = rowStart; i < rowEnd; i++) {
-		allValues.push(this.cells[i].getValue());
-	}
-	return allValues;
+  for (var i = rowStart; i < rowEnd; i++) {
+    allValues.push(this.cells[i].getValue());
+  }
+  return allValues;
 };
 
 Board.prototype.getCurrentMatrixValues = function getCurrentMatrixValues() {
-	var allValues = [];
-	var row = Math.floor(this.selectedIndex / 9);
-	var column = this.selectedIndex % 9;
+  var allValues = [];
+  var row = Math.floor(this.selectedIndex / 9);
+  var column = this.selectedIndex % 9;
 
-	var matrixStart = (parseInt(row / 3) * 3) * 9;
-	var matrixEnd = matrixStart + (3 * 9);
-	var columnShift = parseInt(column / 3) * 3;
+  var matrixStart = (parseInt(row / 3) * 3) * 9;
+  var matrixEnd = matrixStart + (3 * 9);
+  var columnShift = parseInt(column / 3) * 3;
 
-	for (var rowIndex = matrixStart; rowIndex < matrixEnd; rowIndex += 9) {
-		for (var index = rowIndex + columnShift, limit = index + 3; index < limit; index++) {
-			allValues.push(this.cells[index].getValue());
-		}
-	}
+  for (var rowIndex = matrixStart; rowIndex < matrixEnd; rowIndex += 9) {
+    for (var index = rowIndex + columnShift, limit = index + 3; index < limit; index++) {
+      allValues.push(this.cells[index].getValue());
+    }
+  }
 
-	return allValues;
+  return allValues;
 };
 
 Board.prototype.getValue = function getValue(col, row) {
-	col = col - 1;
-	row = row - 1;
-	var indexMatrix = parseInt(row / 3) * 3 + parseInt(col / 3);
-	var indexCell = (row % 3) * 3 + (col % 3);
+  col = col - 1;
+  row = row - 1;
+  var indexMatrix = parseInt(row / 3) * 3 + parseInt(col / 3);
+  var indexCell = (row % 3) * 3 + (col % 3);
 
-	return this.cells[indexMatrix].getValue(indexCell);
+  return this.cells[indexMatrix].getValue(indexCell);
 };
 
 Board.prototype.setValue = function setValue(col, row, value) {
-	col = col - 1;
-	row = row - 1;
+  col = col - 1;
+  row = row - 1;
 
-	var indexMatrix = parseInt(row / 3) * 3 + parseInt(col / 3);
-	var indexCell = (row % 3) * 3 + (col % 3);
+  var indexMatrix = parseInt(row / 3) * 3 + parseInt(col / 3);
+  var indexCell = (row % 3) * 3 + (col % 3);
 
-	if (typeof this.cells[indexMatrix] === 'undefined') {
-		throw new Error('Invalid row/column given. Provide numbers between 1 - 9');
-	}
+  if (typeof this.cells[indexMatrix] === 'undefined') {
+    throw new Error('Invalid row/column given. Provide numbers between 1 - 9');
+  }
 
-	var matrix = this.cells[indexMatrix];
+  var matrix = this.cells[indexMatrix];
 
-	try {
-		matrix.setValue(indexCell, value);
-	} catch (err) {
-		throw new Error('Invalid row/column given. Provide numbers between 1 - 9');
-	}
+  try {
+    matrix.setValue(indexCell, value);
+  } catch (err) {
+    throw new Error('Invalid row/column given. Provide numbers between 1 - 9');
+  }
 };
 
 Board.prototype.updateView = function updateView() {
-	var css = '';
-	if (this.selectedIndex !== null) {
-		var currentValue = this.cells[this.selectedIndex].getValue();
-		var matches = this.boardElem.className.match(/current-selected-value-\d+/);
-		var currentCssClass = matches ? matches[0] : null;
-	}
+  var css = '';
+  if (this.selectedIndex !== null) {
+    var currentValue = this.cells[this.selectedIndex].getValue();
+    var matches = this.boardElem.className.match(/current-selected-value-\d+/);
+    var currentCssClass = matches ? matches[0] : null;
+  }
 
-	if (currentCssClass) {
-		this.boardElem.classList.remove(currentCssClass);
-	}
+  if (currentCssClass) {
+    this.boardElem.classList.remove(currentCssClass);
+  }
 
-	if (currentValue) {
-		this.boardElem.classList.add('current-selected-value-' + currentValue);
-	}
+  if (currentValue) {
+    this.boardElem.classList.add('current-selected-value-' + currentValue);
+  }
 
-	if (this.resolved === true) {
-		var wrapper = this.containerElem.parentNode;
-		wrapper.innerHTML = '<div class="game-resolved-overlay">Completed!</div>' +
-			wrapper.innerHTML;
-	}
+  if (this.resolved === true) {
+    var wrapper = this.containerElem.parentNode;
+    wrapper.innerHTML = '<div class="game-resolved-overlay">Completed!</div>' +
+      wrapper.innerHTML;
+  }
 };
 
 Board.prototype.selectCell = function selectCell(index) {
-	if (typeof this.cells[index] == 'undefined') {
-		return;
-	}
+  if (typeof this.cells[index] == 'undefined') {
+    return;
+  }
 
-	this.selectedIndex = parseInt(index);
+  this.selectedIndex = parseInt(index);
 };
 
 Board.prototype.setValueOnSelectedCell = function setValueOnSelectedCell(value) {
-	if (this.selectedIndex === null) {
-		return;
-	}
+  if (this.selectedIndex === null) {
+    return;
+  }
 
-	this.cells[this.selectedIndex].setValue(value);
+  this.cells[this.selectedIndex].setValue(value);
 };
 
 Board.prototype.clearSelectedCell = function clearSelectedCell() {
-	if (this.selectedIndex === null) {
-		return; // Ignore
-	}
+  if (this.selectedIndex === null) {
+    return; // Ignore
+  }
 
-	this.getSelectedCell().clear();
+  this.getSelectedCell().clear();
 };
 
 Board.prototype.isComplete = function isComplete() {
-	for (var i = 0, l = this.cells.length; i < l; i++) {
-		if (this.cells[i].getValue() === null) {
-			return false;
-		}
-	}
+  for (var i = 0, l = this.cells.length; i < l; i++) {
+    if (this.cells[i].getValue() === null) {
+      return false;
+    }
+  }
 
-	return true;
+  return true;
 };
 
 Board.prototype.markAsResolved = function markAsResolved() {
-	this.resolved = true;
+  this.resolved = true;
 };
 
 Board.prototype.getSelectedCell = function getSelectedCell(topic) {
-	return this.selectedIndex in this.cells ? this.cells[this.selectedIndex] : null;
+  return this.selectedIndex in this.cells ? this.cells[this.selectedIndex] : null;
 };
 
 Board.prototype.onClearKeyPressed = function onClearKeyPressed() {
-	this.clearSelectedCell();
-	this.updateView();
+  this.clearSelectedCell();
+  this.updateView();
 };
 
 Board.prototype.onNumberKeyPressed = function onNumberKeyPressed(topic, pressedNumber) {
@@ -187,16 +187,16 @@ Board.prototype.onNumberKeyPressed = function onNumberKeyPressed(topic, pressedN
 };
 
 Board.prototype.onBoardClicked = function onBoardClicked(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	var cellElem;
-	if (!(cellElem = event.target).classList.contains('sudoku-cell')
-		|| !('index' in cellElem.dataset)) {
-			return;
-	}
+  var cellElem;
+  if (!(cellElem = event.target).classList.contains('sudoku-cell')
+    || !('index' in cellElem.dataset)) {
+      return;
+  }
 
-	this.selectCell(cellElem.dataset.index);
-	this.updateView();
+  this.selectCell(cellElem.dataset.index);
+  this.updateView();
 };
 
 module.exports = Board;
